d73a5ab6a74acc14ad4b1c8f1827a3b85d26c693
AJ <63066039+ajworks13@users.noreply.github.com>
2022-06-20 19:37:16 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+COLORS:
+red: B25068
+blueish: 51557E
+light blue/grayish: AEDBCE
+
+NOTES:
+  TIPS, About Us, Achievements.
+  
+  Buttons highlight/clicked red, unhightlighted is light blue grayish.
+  
+*/
